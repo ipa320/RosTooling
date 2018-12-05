14ba49d7bd0e36687523878aa162c3d942856e6b
ipa-nhg <nhg@ipa.fhg.de>
2018-12-05 17:46:42 +0100

    add toolbar buttons to create new nodes, components, projects and systems

---
+++ de.fraunhofer.ipa.ros.editor/src/ros/presentation/AddRosProjectHandler.java
@@ -0,0 +1,34 @@
+package ros.presentation;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.StructuredSelection;
+import org.eclipse.jface.wizard.WizardDialog;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.KeyEvent;
+import org.eclipse.swt.events.KeyListener;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.handlers.HandlerUtil;
+
+public class AddRosProjectHandler extends AbstractHandler {
+
+    @Override
+    public Object execute(ExecutionEvent event) {
+    	Shell activeShell = HandlerUtil.getActiveShell(event);
+    	RosArtifactWizard wizard = new RosArtifactWizard();
+    	wizard.init(PlatformUI.getWorkbench(), new StructuredSelection());
+    	WizardDialog dialog = new WizardDialog(activeShell,wizard);
+    	dialog.create();
+    	dialog.getShell().setText(wizard.getWindowTitle());
+    	int result = dialog.open();
+        return null;
+    }
+
+}
