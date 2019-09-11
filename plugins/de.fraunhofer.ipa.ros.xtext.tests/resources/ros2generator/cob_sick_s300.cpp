f8959930f87bc3e66830741ed6f94107626c6926
ipa-nhg <nadia.hammoudeh.garcia@ipa.fraunhofer.de>
2019-09-11 11:36:35 +0200

    fix ros2 generated code

--- plugins/de.fraunhofer.ipa.ros.xtext.tests/resources/ros2generator/cob_sick_s300.cpp
+++ plugins/de.fraunhofer.ipa.ros.xtext.tests/resources/ros2generator/cob_sick_s300.cpp
@@ -6,9 +6,14 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include "rcutils/cmdline_parser.h"
-#include <sensor_msgs/msg/LaserScan.hpp>
-#include <std_msgs/msg/Bool.hpp>
-#include <diagnostic_msgs/msg/DiagnosticArray.hpp>
+#include <sensor_msgs/msg/laser_scan.hpp>
+#include <std_msgs/msg/bool.hpp>
+#include <diagnostic_msgs/msg/diagnostic_array.hpp>
+
+using namespace std::chrono_literals;
+using std::placeholders::_1;
+using std::placeholders::_2;
+using std::placeholders::_3;
 
 void print_usage()
 {
@@ -16,63 +21,48 @@ void print_usage()
   printf("..... \n");
   printf("..... \n");
   printf("..... \n");
-
 }
 
   class cob_sick_s300 : public rclcpp::Node {
-  	public:
-	  explicit cob_sick_s300(const std::string & scan scan_standby /diagnostics ) : Node("cob_sick_s300")
-	  auto publish_message = [this]() -> void {
-    	msg_scan_ = std::make_unique<sensor_msgs::msg::LaserScan>();
-    	scan_->publish(std::move(msg_scan_));
-    	msg_scan_standby_ = std::make_unique<std_msgs::msg::Bool>();
-    	scan_standby_->publish(std::move(msg_scan_standby_));
-    	msg_/diagnostics_ = std::make_unique<diagnostic_msgs::msg::DiagnosticArray>();
-    	/diagnostics_->publish(std::move(msg_/diagnostics_));
-	  };
+    public:
+      cob_sick_s300() : Node("cob_sick_s300") {
+        scan_ = this->create_publisher<sensor_msgs::msg::LaserScan>("scan",10);
+        scan_standby_ = this->create_publisher<std_msgs::msg::Bool>("scan_standby",10);
+        diagnostics_ = this->create_publisher<diagnostic_msgs::msg::DiagnosticArray>("/diagnostics",10);
 
-	  rclcpp::QoS qos(rclcpp::KeepLast(7));
-scan_ = this->create_publisher<sensor_msgs::msg::LaserScan>(scan, qos);
-scan_standby_ = this->create_publisher<std_msgs::msg::Bool>(scan_standby, qos);
-/diagnostics_ = this->create_publisher<diagnostic_msgs::msg::DiagnosticArray>(/diagnostics, qos);
-}
+        timer_ = this->create_wall_timer(500ms, std::bind(&cob_sick_s300::timer_callback, this));
 
-private:
-  size_t count_ = 1;
-  std::unique_ptr<sensor_msgs::msg::LaserScan> msg_scan_;
-  rclcpp::Publisher<sensor_msgs::msg::LaserScan>::SharedPtr scan_;
-  std::unique_ptr<std_msgs::msg::Bool> msg_scan_standby_;
-  rclcpp::Publisher<std_msgs::msg::Bool>::SharedPtr scan_standby_;
-  std::unique_ptr<diagnostic_msgs::msg::DiagnosticArray> msg_/diagnostics_;
-  rclcpp::Publisher<diagnostic_msgs::msg::DiagnosticArray>::SharedPtr /diagnostics_;
-  rclcpp::TimerBase::SharedPtr timer_;
+      }
+
+    private:
+      rclcpp::Publisher<sensor_msgs::msg::LaserScan>::SharedPtr scan_;
+      rclcpp::Publisher<std_msgs::msg::Bool>::SharedPtr scan_standby_;
+      rclcpp::Publisher<diagnostic_msgs::msg::DiagnosticArray>::SharedPtr diagnostics_;
+      // Timer Callback
+      void timer_callback(){
+        auto scan_msg = sensor_msgs::msg::LaserScan();
+        //scan_msg = ...
+        scan_->publish(scan_msg);
+        RCLCPP_INFO(this->get_logger(), "scan publisher active");
+        auto scan_standby_msg = std_msgs::msg::Bool();
+        //scan_standby_msg = ...
+        scan_standby_->publish(scan_standby_msg);
+        RCLCPP_INFO(this->get_logger(), "scan_standby publisher active");
+        auto diagnostics_msg = diagnostic_msgs::msg::DiagnosticArray();
+        //diagnostics_msg = ...
+        diagnostics_->publish(diagnostics_msg);
+        RCLCPP_INFO(this->get_logger(), "/diagnostics publisher active");
+      }
+      rclcpp::TimerBase::SharedPtr timer_;
+
+      
 };
 
 int main(int argc, char * argv[])
 {
-  setvbuf(stdout, NULL, _IONBF, BUFSIZ);
-
-  if (rcutils_cli_option_exist(argv, argv + argc, "-h")) {
-    print_usage();
-    return 0;
-  }
-
   rclcpp::init(argc, argv);
-  auto scan = std::string("scan");
-  auto scan_standby = std::string("scan_standby");
-  auto /diagnostics = std::string("/diagnostics");
-  char * cli_option = rcutils_cli_get_option(argv, argv + argc, "-t");
-  if (nullptr != cli_option) {
-  scan = std::string(cli_option);
-  scan_standby = std::string(cli_option);
-  /diagnostics = std::string(cli_option);
-  }
-   auto node = std::make_shared<cob_sick_s300>(scan)
-   auto node = std::make_shared<cob_sick_s300>(scan_standby)
-   auto node = std::make_shared<cob_sick_s300>(/diagnostics)
-
-  rclcpp::spin(cob_sick_s300);
-
+  rclcpp::spin(std::make_shared<cob_sick_s300>());
   rclcpp::shutdown();
   return 0;
 }
+
