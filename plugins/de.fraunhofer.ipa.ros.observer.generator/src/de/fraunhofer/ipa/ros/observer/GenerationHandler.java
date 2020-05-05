0eb081df696b681ea3690e47058a104ab375b6a8
ipa-nhg <nadia.hammoudeh.garcia@ipa.fraunhofer.de>
2020-05-05 16:16:53 +0200

    add first working version of the topics observer generator

---
+++ plugins/de.fraunhofer.ipa.ros.observer.generator/src/de/fraunhofer/ipa/ros/observer/GenerationHandler.java
@@ -0,0 +1,219 @@
+package de.fraunhofer.ipa.ros.observer;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.commands.IHandler;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.dialogs.ElementListSelectionDialog;
+import org.eclipse.ui.handlers.HandlerUtil;
+import org.eclipse.xtext.builder.EclipseResourceFileSystemAccess2;
+import org.eclipse.xtext.generator.GeneratorContext;
+import org.eclipse.xtext.generator.IOutputConfigurationProvider;
+import org.eclipse.xtext.generator.OutputConfiguration;
+import org.eclipse.xtext.resource.IResourceDescriptions;
+import org.eclipse.xtext.ui.resource.IResourceSetProvider;
+
+import com.google.inject.Inject;
+import com.google.inject.Provider;
+
+import componentInterface.ComponentInterface;
+import componentInterface.RosPublisher;
+import de.fraunhofer.ipa.ros.observer.generator.CustomOutputProvider;
+import de.fraunhofer.ipa.ros.observer.generator.ObserverPyCodeGenerator;
+import ros.Subscriber;
+import ros.impl.SubscriberImpl;
+import rossystem.RosSystem;
+
+public class GenerationHandler extends AbstractHandler implements IHandler {
+	
+	@Inject
+	private Provider<EclipseResourceFileSystemAccess2> fileAccessProvider;
+	
+	@Inject
+	IResourceDescriptions resourceDescriptions;
+	
+	@Inject
+	IResourceSetProvider resourceSetProvider;
+
+	static Map<String, OutputConfiguration> getOutputConfigurationsAsMap(IOutputConfigurationProvider provider) {
+		Map<String, OutputConfiguration> outputs = new HashMap<String, OutputConfiguration>();
+		for(OutputConfiguration c: provider.getOutputConfigurations()) {
+			outputs.put(c.getName(), c);
+		}
+		return outputs;
+	}
+
+	@SuppressWarnings("null")
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		ISelection selection = HandlerUtil.getCurrentSelection(event);
+		if (selection instanceof IStructuredSelection) {
+			IStructuredSelection structuredSelection = (IStructuredSelection) selection;
+			Object firstElement = structuredSelection.getFirstElement();
+			if (firstElement instanceof IFile) {
+			IFile file = (IFile) firstElement;
+			IProject project = file.getProject();
+
+			URI uri = URI.createPlatformResourceURI(file.getFullPath().toString(), true);
+			ResourceSet rs = resourceSetProvider.get(project);
+			Resource r = rs.getResource(uri, true);
+				
+			Display display = Display.getDefault();
+			Shell shell = display.getActiveShell();
+
+			RosSystem rossystem = (RosSystem)r.getContents().get(0);
+			EList<ComponentInterface> roscomponents = rossystem.getRosComponent();
+			List<EObject> RosInterfaces = new ArrayList<EObject>();
+			for (int i=0; i<roscomponents.size(); i++) {
+				List<EObject>  NewRosInterfaces = getInterfaces(roscomponents.get(i));
+				for (int j= 0;j<NewRosInterfaces.size();j++) {
+					RosInterfaces.add(NewRosInterfaces.get(j));
+				}
+			}
+
+			ElementListSelectionDialog dialogSelect = new ElementListSelectionDialog(shell, new LabelProvider());
+		
+			String[] ListofInterfaces = new String[RosInterfaces.size()];
+			for (int i=0; i<RosInterfaces.size(); i++) {
+				ListofInterfaces[i]=(getViewMenuInterfaceName(RosInterfaces.get(i)));
+			}
+
+			dialogSelect.setElements(ListofInterfaces);
+			dialogSelect.setTitle("Select the ROS interfaces to observe");
+			dialogSelect.setMultipleSelection(true);
+			dialogSelect.open();
+			Object[] results = dialogSelect.getResult();
+			String observer_name = "my_observer";
+			String RelativePathToObserverModel = "src-gen/"+observer_name+".ros";
+
+			IFile ObserverModelFile = project.getFile(RelativePathToObserverModel);
+			
+			String ros_model =  
+					"PackageSet { package { \n" + 
+					"  CatkinPackage rosgraph_monitor { artifact {\n" + 
+					"    Artifact "+observer_name+" {\n" + 
+					"      node Node { name /"+observer_name+"\n";
+			ros_model+="        publisher { \n"+
+					"          Publisher { name '/diagnostics' message 'diagnostic_msgs.DiagnosticArray'}}\n";
+			List<Subscriber> rossubscribers = new ArrayList<Subscriber>();
+
+			for (Object result_: results) {
+				for (EObject ResultInterface: RosInterfaces) {
+					if((getViewMenuInterfaceName(ResultInterface)).equals(result_.toString())) {
+						EObject SelectedInterface = ResultInterface;
+
+						if (SelectedInterface.getClass().toString().contains("componentInterface.impl.RosPublisherImpl")) {
+							Subscriber sub = new SubscriberImpl();
+							sub.setName(((RosPublisher) SelectedInterface).getName());
+							sub.setMessage(((RosPublisher)SelectedInterface).getPublisher().getMessage());
+							rossubscribers.add(sub);
+						}
+					}}}
+			if (!rossubscribers.isEmpty()) {
+				ros_model+="        subscriber {\n";
+				for(int i=0;i<rossubscribers.size();i++) {
+					ros_model+="          Subscriber { name '"+rossubscribers.get(i).getName()+"' message '"+rossubscribers.get(i).getMessage().getFullname().replace("/", ".")+"'},\n";
+				}
+				ros_model=ros_model.substring(0, ros_model.length() - 2);
+				ros_model+="}";
+			}
+			ros_model+="}}}}}}";
+
+
+			byte[] bytes = (ros_model).getBytes();
+				
+			InputStream source = new ByteArrayInputStream(bytes);
+			try {
+				if (!ObserverModelFile.exists()) {
+					ObserverModelFile.create(source, IResource.NONE, null);
+				} else{
+					@SuppressWarnings("resource")
+					OutputStream outputStream = new FileOutputStream(new File(project.getLocation().toString()+"/"+RelativePathToObserverModel));
+					outputStream.write(bytes);
+				}
+
+			} catch (CoreException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			final EclipseResourceFileSystemAccess2 fsa = fileAccessProvider.get();
+			fsa.setProject(project);
+			fsa.setOutputConfigurations(getOutputConfigurationsAsMap(new CustomOutputProvider()));
+			fsa.setMonitor(new NullProgressMonitor()); 
+
+			URI uriObserverFile = URI.createPlatformResourceURI(ObserverModelFile.getFullPath().toString(), true);
+			ResourceSet rs2 = resourceSetProvider.get(project);
+			Resource r2 = rs2.getResource(uriObserverFile, true);
+			ObserverPyCodeGenerator generator = new ObserverPyCodeGenerator();
+			generator.doGenerate(r2, fsa, new GeneratorContext());
+		}}
+		return null;
+	}
+
+	private List<EObject> getInterfaces(ComponentInterface componentInterface_model) {
+		List<EObject> ROSInterfaces = new ArrayList<EObject>();
+		for (RosPublisher RosPub: componentInterface_model.getRospublisher()) {
+			ROSInterfaces.add(RosPub);
+		}
+		/**for (RosSubscriber RosSub: componentInterface_model.getRossubscriber()) {
+			ROSInterfaces.add(RosSub);
+		}
+		for (RosServiceClient RosSrvc: componentInterface_model.getRosserviceclient()) {
+			ROSInterfaces.add(RosSrvc);
+		}
+		for (RosServiceServer RosSrvs: componentInterface_model.getRosserviceserver()) {
+			ROSInterfaces.add(RosSrvs);
+		}
+		for (RosActionClient RosActc: componentInterface_model.getRosactionclient()) {
+			ROSInterfaces.add(RosActc);
+		}
+		for (RosActionServer RosActs: componentInterface_model.getRosactionserver()) {
+			ROSInterfaces.add(RosActs);
+		}*/
+		return ROSInterfaces;
+	}
+	
+	private String getInterfaceName(EObject RosInterface) {
+		String name = RosInterface.toString().substring(RosInterface.toString().indexOf("name:")+6,RosInterface.toString().indexOf(","));
+		return name;
+	}
+	private String getViewMenuInterfaceName(EObject RosInterface) {
+		String name = "["+RosInterface.toString().substring(RosInterface.toString().indexOf("impl.Ros")+8,RosInterface.toString().indexOf("Impl@"))+"]  "+
+				getInterfaceName(RosInterface);
+		return name;
+	}
+	
+	@Override
+	  public boolean isEnabled() {
+		return true;
+	  }
+	}
---
+++ plugins/de.fraunhofer.ipa.ros.observer.generator/src/de/fraunhofer/ipa/ros/observer/generator/ObserverPyCodeGenerator.xtend
@@ -0,0 +1,85 @@
+package de.fraunhofer.ipa.ros.observer.generator
+
+import org.eclipse.emf.ecore.resource.Resource
+import org.eclipse.xtext.generator.AbstractGenerator
+import org.eclipse.xtext.generator.IFileSystemAccess2
+import org.eclipse.xtext.generator.IGeneratorContext
+import org.eclipse.xtext.generator.IOutputConfigurationProvider
+import org.eclipse.xtext.generator.OutputConfiguration
+import java.util.Set
+import ros.Node
+
+class CustomOutputProvider implements IOutputConfigurationProvider {
+	public final static String OBSERVER_OUTPUT = "OBSERVER_OUTPUT"
+	
+	override Set<OutputConfiguration> getOutputConfigurations() {
+		var OutputConfiguration observer_config = new OutputConfiguration(OBSERVER_OUTPUT)
+		observer_config.setDescription("OBSERVER_OUTPUT");
+		observer_config.setOutputDirectory("./src-gen/observers/");
+		observer_config.setOverrideExistingResources(true);
+		observer_config.setCreateOutputDirectory(true);
+		observer_config.setCleanUpDerivedResources(true);
+		observer_config.setSetDerivedProperty(true);
+		return newHashSet(observer_config)
+	}
+}
+
+class ObserverPyCodeGenerator extends AbstractGenerator {
+
+	int count_sub
+
+	override void doGenerate(Resource resource, IFileSystemAccess2 fsa, IGeneratorContext context) {
+			for (node : resource.allContents.toIterable.filter(Node)){
+				fsa.generateFile(node.getName()+".py",CustomOutputProvider::OBSERVER_OUTPUT,node.compile)
+				}
+			}
+
+def compile(Node node) {
+	count_sub = node.subscriber.size
+'''
+from rosgraph_monitor.observer import TopicObserver
+«FOR sub:node.subscriber»
+from «sub.message.package.name».msg import «sub.message.name»
+«ENDFOR»
+from diagnostic_msgs.msg import DiagnosticStatus, KeyValue
+
+
+class QualityObserver(TopicObserver):
+    def __init__(self, name):
+    	
+        topics = [
+«FOR sub:node.subscriber»«val count_sub=count_sub--»
+        ("«sub.name»", «sub.message.name»)«IF count_sub > 1 »,«ENDIF»«ENDFOR»
+        ]
+
+        super(QualityObserver, self).__init__(
+            name, 10, topics)
+
+    def calculate_attr(self, msgs):
+        status_msg = DiagnosticStatus()
+        
+        #Write here your code (or uncomment the following lines)
+        #if len(msgs) < 2:
+        #    print("Incorrect number of messages")
+        #    return status_msg
+        
+        #if not isinstance(msgs[0], Int32) or not isinstance(msgs[1], Int32):
+        #    print("Incorrect instance of message")
+        #   return status_msg
+
+        #attr = msgs[0].data + msgs[1].data
+        #print("{0} + {1}".format(msgs[0].data, msgs[1].data))
+
+        status_msg = DiagnosticStatus()
+        status_msg.level = DiagnosticStatus.OK
+        status_msg.name = self._id
+        # Set here your name attribute
+        status_msg.values.append(
+            KeyValue("KeyName", attr))
+        status_msg.message = "QA status"
+
+        return status_msg
+'''
+}
+
+}
