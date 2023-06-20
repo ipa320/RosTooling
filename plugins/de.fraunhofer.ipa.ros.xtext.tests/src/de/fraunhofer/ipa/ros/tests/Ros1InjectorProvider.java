03a393d1cc41586f2104fddbbe3e7d747848346e
ipa-nhg <nadia.hammoudeh.garcia@ipa.fraunhofer.de>
2023-06-20 16:00:12 +0200

    update test package: create language parsers tests

---
+++ plugins/de.fraunhofer.ipa.ros.xtext.tests/src/de/fraunhofer/ipa/ros/tests/Ros1InjectorProvider.java
@@ -0,0 +1,67 @@
+package de.fraunhofer.ipa.ros.tests;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import de.fraunhofer.ipa.ros1.Ros1RuntimeModule;
+import de.fraunhofer.ipa.ros1.Ros1StandaloneSetup;
+
+import org.eclipse.xtext.testing.GlobalRegistries;
+import org.eclipse.xtext.testing.GlobalRegistries.GlobalStateMemento;
+import org.eclipse.xtext.testing.IInjectorProvider;
+import org.eclipse.xtext.testing.IRegistryConfigurator;
+
+public class Ros1InjectorProvider implements IInjectorProvider, IRegistryConfigurator {
+
+	protected GlobalStateMemento stateBeforeInjectorCreation;
+	protected GlobalStateMemento stateAfterInjectorCreation;
+	protected Injector injector;
+
+	static {
+		GlobalRegistries.initializeDefaults();
+	}
+
+	@Override
+	public Injector getInjector() {
+		if (injector == null) {
+			this.injector = internalCreateInjector();
+			stateAfterInjectorCreation = GlobalRegistries.makeCopyOfGlobalState();
+		}
+		return injector;
+	}
+
+	protected Injector internalCreateInjector() {
+		return new Ros1StandaloneSetup() {
+			@Override
+			public Injector createInjector() {
+				return Guice.createInjector(createRuntimeModule());
+			}
+		}.createInjectorAndDoEMFRegistration();
+	}
+
+	protected Ros1RuntimeModule createRuntimeModule() {
+		// make it work also with Maven/Tycho and OSGI
+		// see https://bugs.eclipse.org/bugs/show_bug.cgi?id=493672
+		return new Ros1RuntimeModule() {
+			@Override
+			public ClassLoader bindClassLoaderToInstance() {
+				return Ros1InjectorProvider.class
+						.getClassLoader();
+			}
+		};
+	}
+
+	@Override
+	public void restoreRegistry() {
+		stateBeforeInjectorCreation.restoreGlobalState();
+		stateBeforeInjectorCreation = null;
+	}
+
+	@Override
+	public void setupRegistry() {
+		stateBeforeInjectorCreation = GlobalRegistries.makeCopyOfGlobalState();
+		if (injector == null) {
+			getInjector();
+		}
+		stateAfterInjectorCreation.restoreGlobalState();
+	}
+}
---
+++ plugins/de.fraunhofer.ipa.ros.xtext.tests/src/de/fraunhofer/ipa/ros/tests/Ros2InjectorProvider.java
@@ -0,0 +1,67 @@
+package de.fraunhofer.ipa.ros.tests;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import de.fraunhofer.ipa.ros2.Ros2RuntimeModule;
+import de.fraunhofer.ipa.ros2.Ros2StandaloneSetup;
+
+import org.eclipse.xtext.testing.GlobalRegistries;
+import org.eclipse.xtext.testing.GlobalRegistries.GlobalStateMemento;
+import org.eclipse.xtext.testing.IInjectorProvider;
+import org.eclipse.xtext.testing.IRegistryConfigurator;
+
+public class Ros2InjectorProvider implements IInjectorProvider, IRegistryConfigurator {
+
+	protected GlobalStateMemento stateBeforeInjectorCreation;
+	protected GlobalStateMemento stateAfterInjectorCreation;
+	protected Injector injector;
+
+	static {
+		GlobalRegistries.initializeDefaults();
+	}
+
+	@Override
+	public Injector getInjector() {
+		if (injector == null) {
+			this.injector = internalCreateInjector();
+			stateAfterInjectorCreation = GlobalRegistries.makeCopyOfGlobalState();
+		}
+		return injector;
+	}
+
+	protected Injector internalCreateInjector() {
+		return new Ros2StandaloneSetup() {
+			@Override
+			public Injector createInjector() {
+				return Guice.createInjector(createRuntimeModule());
+			}
+		}.createInjectorAndDoEMFRegistration();
+	}
+
+	protected Ros2RuntimeModule createRuntimeModule() {
+		// make it work also with Maven/Tycho and OSGI
+		// see https://bugs.eclipse.org/bugs/show_bug.cgi?id=493672
+		return new Ros2RuntimeModule() {
+			@Override
+			public ClassLoader bindClassLoaderToInstance() {
+				return Ros2InjectorProvider.class
+						.getClassLoader();
+			}
+		};
+	}
+
+	@Override
+	public void restoreRegistry() {
+		stateBeforeInjectorCreation.restoreGlobalState();
+		stateBeforeInjectorCreation = null;
+	}
+
+	@Override
+	public void setupRegistry() {
+		stateBeforeInjectorCreation = GlobalRegistries.makeCopyOfGlobalState();
+		if (injector == null) {
+			getInjector();
+		}
+		stateAfterInjectorCreation.restoreGlobalState();
+	}
+}
